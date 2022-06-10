bf66096ef5b13858c402d2ca750d603e07caed0e
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-06-11 03:14:17 +0430

    commit

---
+++ components/navbar.js
@@ -0,0 +1,21 @@
+
+const Nav = () => {
+  
+
+  return (
+    <nav className="navbar navbarBox px-4">
+      <div className="navContainer">
+        <span className="logo">ShadenBooking</span>
+        <div className="navItems">
+       
+              <button className="navButton">Register</button>
+              <button className="navButton">
+                Sign in
+              </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import useSWR from "swr";
+import Nav from "../components/navbar";
+
+const fetcher = async () => {
+  const response = await fetch("http://localhost:3000/api/posts");
+  const data = await response.json();
+  return data;
+};
+
+export default function Home() {
+  const { data, error } = useSWR("http://localhost:3000/api/posts", fetcher);
+  // console.log(data);
+  if (error) return "an error has occured";
+  if (!data) return "Loading";
+
+  const { inf } = data;
+
+  return (
+    <>
+      <Head>
+        <title>shaden booking</title>
+      </Head>
+      <Nav />
+      <div className="featured">
+        <div className="row">
+          {inf.map((e) => 
+            <div className="featuredItem col-sm-4" key={e.id}>
+              <img src={e.thumbnail} alt="ax" className="featuredImg" />
+              <div className="featuredTitles">
+                <h1 className="featuredTitle">{e.name}</h1>
+                <h4 className="featuredTitle">Experiences {e.regionId}</h4>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
