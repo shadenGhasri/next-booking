69d370096925f95abbead7a8cb7402947ede5c99
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-06-12 00:58:43 +0430

    commit

--- pages/signin.js
+++ pages/signin.js
@@ -1,5 +1,20 @@
-const Signin = () => {
-    return (<></>  );
-}
- 
-export default Signin;<></>
+
+import rocketImg from "../assets/rocket.png"
+import Signup from '../components/signup';
+
+function Sigin() {
+    return (
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-md-5">
+            <Signup />
+          </div>
+          <div className="col-md-7 my-auto">
+            <img className="img-fluid w-100" src={rocketImg} alt=""/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Sigin;
